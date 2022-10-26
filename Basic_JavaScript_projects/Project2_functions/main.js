a01d3d87c5285d403227df9ae963dabcc017da9c
Lizzy Esqueda <99945107+LizzyE22@users.noreply.github.com>
2022-10-25 19:46:44 -0700

    Delete Basic_JavaScript_projects/Project2_functions directory

--- Basic_JavaScript_projects/Project2_functions/main.js
+++
@@ -1,8 +0,0 @@
-
-function My_First_Function() {              //Defining a function and naming it
-    var str = 'This is the button text';    //Defining a variable and giving it a string value
-    document.getElementById("Button_Text").innerHTML = str;     //Putting the value of the variable
-                                                                //with the "Button_Text" id
-}
-
-
